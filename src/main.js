d178f67f91cb283841bcbbf7b496340d214f7506
Lulkast <Lulkast@yandex.ru>
2021-01-09 22:57:48 +0300

    + axios

--- src/main.js
+++ src/main.js
@@ -3,6 +3,7 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 import './plugins/bootstrap-vue'
+import './plugins/axios'
 
 Vue.config.productionTip = false
 

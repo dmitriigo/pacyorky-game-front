df248665e6a399e9004efd332de9327538794de7
Lulkast <Lulkast@yandex.ru>
2021-01-10 12:01:39 +0300

    + ukrainian

--- src/plugins/multi-lang.js
+++ src/plugins/multi-lang.js
@@ -4,3 +4,13 @@ import { MLInstaller, MLCreate, MLanguage } from 'vue-multilanguage'
 Vue.use(MLInstaller)
 Vue.use(MLCreate)
 Vue.use(MLanguage)
+
+export default new MLCreate({
+    initial: 'Ukrainian',
+    save: process.env.NODE_ENV === 'production',
+    languages: [
+        new MLanguage('Ukrainian').create({
+
+        })
+    ]
+})

15e6c24e8114722c161abae1ce644edc6c4f37f4
Lulkast <Lulkast@yandex.ru>
2021-01-10 19:32:31 +0300

    fuckup

---
+++ src/router.js
@@ -0,0 +1,19 @@
+import Vue from "vue";
+import Router from 'vue-router'
+import PacyorkyGameDesktopApplication from "./components/desktopApp/PacyorkyGameDesktopApplication";
+import PacyorkyGameMobileApplication from "./components/mobileApp/PacyorkyGameMobileApplication";
+
+
+Vue.use(Router)
+
+export default new Router ({
+    mode: 'history',
+    routes: [
+        {
+            path: '',
+            components: {
+                desktop: PacyorkyGameDesktopApplication,
+                mobile: PacyorkyGameMobileApplication
+            }
+        }]
+})
